20f586e465f3dd2fa8cac712e471d2d72a089a73
priyam1234-spec <76897482+priyam1234-spec@users.noreply.github.com>
2021-09-15 10:18:09 +0530

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -8,11 +8,11 @@ module.exports = {
 
     let inviteEmbed = new MessageEmbed()
       .setTitle("Add us to your server!")
-      .setDescription("Love using Harmony? Great, Thank you! Consider adding it to your server")
+      .setDescription("Love using The Music Bot? Great, Thank you! Consider adding it to your server")
       .setColor("#F0EAD6")
-      .setAuthor('Harmony','https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif')
+      .setAuthor('Music Bot','https://avatars.githubusercontent.com/u/76640873?s=200&v=4')
       .setThumbnail(message.guild.iconURL())
-      .addField(`Use the following link to add Harmony to your discord server`, 'https://discord.com/api/oauth2/authorize?client_id=767885987740254291&permissions=49572160&scope=bot', true)
+      .addField(`Use the following link to add Music Bot to your discord server`, 'https://discord.com/api/oauth2/authorize?client_id=887557968705519706&permissions=8&scope=bot%20applications.commands', true)
 
     inviteEmbed.setTimestamp();
 
